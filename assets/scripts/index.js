4891c08ec38cc2b2da41cf77632d995a01937276
Yemata Ferry <yemataferry@gmail.com>
2024-01-03 17:43:26 +0100

    linter errors arranged

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,6 @@
 const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
 
-toggle.addEventListener('click', () => nav.classList .toggle('active'))
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
